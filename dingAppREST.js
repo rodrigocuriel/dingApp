161106fdf0f493c9eb1e6ed3bca8b69430a05a6b
Rodrigo Curiel <rodrigo.salazar@onstar.com>
2014-09-29 15:24:13 -0400

    Rodrigo Curiel - Added REST for LED, changed port

--- dingAppREST.js
+++ dingAppREST.js
@@ -1,7 +1,3 @@
-// server.js
-// BASE SETUP
-// =============================================================================
-
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -10,7 +6,6 @@ var arduinoBoard = new five.Board();
 var ledPIN = {};
 var bellPIN = {};
 
-// on board ready
 arduinoBoard.on("ready", function() {
     bellPIN = new five.Led(2);
     ledPIN = new five.Led(8).strobe(1000);
@@ -21,7 +16,7 @@ arduinoBoard.on("ready", function() {
 app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8110; 		// set our port
+var port = process.env.PORT || 8210; 		// set our port
 
 // ROUTES FOR OUR DING API
 // =============================================================================
@@ -30,18 +25,16 @@ var router = express.Router(); 				// get an instance of the express Router
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/dingApp)
+// test route to make sure everything is working (accessed at GET http://localhost:<PORT>/dingApp)
 router.get('/', function(req, res) {
     res.json({ message : 'hooray! welcome to Code Review!' });
 });
 
+// create a POST (accessed at POST http://localhost:<PORT>/dingApp/bell)
 router.route('/bell')
-
-    // create a POST (accessed at POST http://localhost:8110/dingApp/bell)
     .post(function(req, res) {
         if(arduinoBoard.isReady) {
             bellPIN.on();
@@ -49,6 +42,14 @@ router.route('/bell')
                 bellPIN.off();
             }, req.body.delay);
             res.json({ message : 'Bell dinged!' });
+        }
+    });
+
+router.route('/led')
+    .post(function(req, res) {
+        console.log("Changing LED strobe delay to", data.delay);
+        if(board.isReady) {
+            led.strobe(req.body.delay);
         }
     });
 
